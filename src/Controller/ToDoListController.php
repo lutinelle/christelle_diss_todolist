09311b077f574e368cddffa3e3aee4c115248a5a
christelle diss <lutinelle01@yahoo.fr>
2022-05-12 17:57:46 +0200

    remove modal and finish crud list + initiate crud task

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends AbstractController
+class ToDoListController extends AbstractController
 {
     #[Route('/', name: 'index')]
     public function readList(ToDoListRepository $toDoListRepository ): Response
@@ -20,66 +20,66 @@ class DefaultController extends AbstractController
 
         $lists = $toDoListRepository->findAll();
 
-        return $this->render('default/index.html.twig', [
+        return $this->render('to_do_list/index.html.twig', [
             'controller_name' => 'DefaultController',
             'lists'=>$lists
         ]);
     }
 
-    #[Route('/create', name: 'create')]
-    public function addList(EntityManagerInterface $em, Request $request,
-                             ToDoListRepository $toDoListRepository ): Response
+    #[Route('/addList', name: 'addList')]
+    public function addList(EntityManagerInterface $em, Request $request,): Response
     {
         //create
         $list = new ToDoList();
-
         $formList = $this->createForm(ToDoListType::class,$list);
         $formList->handleRequest($request);
 
+        // submit
         if ($formList->isSubmitted() && $formList->isValid()) {
             $em->persist($list);
             $em->flush();
+            return $this->redirectToRoute('index');
         }
 
-        //read
-        $lists = $toDoListRepository->findAll();
-
-
-
-
-
         //render
-        return $this->render('default/addList.html.twig', [
+        return $this->render('to_do_list/addList.html.twig', [
             'controller_name' => 'modale',
             'formList' => $formList->createView(),
-            'lists'=>$lists
         ]);
     }
-    #[Route('/update', name: 'update')]
-    public function updateList(EntityManagerInterface $em, Request $request,
-                            ToDoListRepository $toDoListRepository ): Response
+
+    #[Route('/updateList/{id}', name: 'updateList')]
+    public function updateList(int $id, EntityManagerInterface $em, Request $request,): Response
     {
 
-        //read
-        $lists = $toDoListRepository->findAll();
-
-
-
         //update
-        $listToUpdate = $em->getRepository(ToDoList::class)->find(1);
+        $listToUpdate = $em->getRepository(ToDoList::class)->find($id);
         $formUpdateList = $this->createForm(ToDoListType::class,$listToUpdate);
         $formUpdateList->handleRequest($request);
 
+        //submit
         if ($formUpdateList->isSubmitted() && $formUpdateList->isValid()) {
-        $em->flush();
+            $em->flush();
+            return $this->redirectToRoute('index');
         }
 
         //render
-        return $this->render('default/updateList.html.twig', [
+        return $this->render('to_do_list/updateList.html.twig', [
             'controller_name' => 'modale',
-            'formList' => $formUpdateList->createView(),
-            'lists'=>$lists
+            'formList' => $formUpdateList->createView()
         ]);
     }
 
+    #[Route('/delList/{id}', name: 'delList')]
+    public function delList(int $id, EntityManagerInterface $em): Response
+    {
+
+        //update
+        $listToDel = $em->getRepository(ToDoList::class)->find($id);
+            em->remove($listToDel);
+            em->flush;
+
+            return $this->redirectToRoute('index');
+
+    }
 }
